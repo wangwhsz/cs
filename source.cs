3920fe74a152812508220a51d01e3c611271d2d6
WangzyaaaA <482673302@qq.com>
2020-09-21 18:56:51 +0800

    a new file

---
+++ source.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //amdyes
+            Console.WriteLine("1");
+            //yes
+        }
+    }
+}
