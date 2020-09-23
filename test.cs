845dad4ec4ffb19cca731662223d09948effca30
WangzyaaaA <482673302@qq.com>
2020-09-23 20:55:35 +0800

    9.23 1

--- test.cs
+++ test.cs
@@ -2,9 +2,29 @@ using System;
 
 namespace Test
 {
+    class Factorial{
+        public int Execute(int n){
+            int result;
+            if(n == 1){
+                return 1;
+            }
+                else {
+                    result = (Execute(n - 1) * n);
+                    return result;
+                }
+        }
+    }
     class Test{
-        static void main(){
-
+         
+        static void Main(){
+            int num;
+            Console.WriteLine("请输入一个整数:");
+            num = Convert.ToInt32(Console.ReadLine());
+            Factorial n = new Factorial();
+            int result;
+            result = n.Execute(num);
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
